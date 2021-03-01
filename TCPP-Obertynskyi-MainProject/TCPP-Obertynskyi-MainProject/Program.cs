e8e9f23582709424e7b79213cb8834da61b3b057
Олександр Обертинський <obertynskyi.o@donnu.edu.ua>
2021-03-01 21:29:22 +0200

    Зміни_2

--- TCPP-Obertynskyi-MainProject/TCPP-Obertynskyi-MainProject/Program.cs
+++ TCPP-Obertynskyi-MainProject/TCPP-Obertynskyi-MainProject/Program.cs
@@ -6,7 +6,7 @@ namespace TCPP_Obertynskyi_MainProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World ALFA!");
             //гілка альфа
         }
     }

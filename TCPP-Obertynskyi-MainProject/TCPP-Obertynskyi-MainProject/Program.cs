ce2cefd88c80250e15f27f6918c6170bb4affa24
Obertynskyi <79458879+Obertynskyi@users.noreply.github.com>
2021-03-01 21:45:25 +0200

    перепутав гілки)

--- TCPP-Obertynskyi-MainProject/TCPP-Obertynskyi-MainProject/Program.cs
+++ TCPP-Obertynskyi-MainProject/TCPP-Obertynskyi-MainProject/Program.cs
@@ -7,7 +7,7 @@ namespace TCPP_Obertynskyi_MainProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World ALFA!");
-            //гілка альфа (бета)
+            //гілка альфа
         }
     }
 }
